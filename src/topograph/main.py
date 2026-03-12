450d6f8a7cb22a4e98d4f15faf4fb7f49caf1a37
Igors Dubanevics <26872977+igorsdub@users.noreply.github.com>
2026-03-12 15:18:20 +0900

    Remove gun CLI example

--- src/topograph/main.py
+++ src/topograph/main.py
@@ -1,16 +1,12 @@
 import typer
 
-from .gun import app as gun_app
+from .transforms import app as transforms_app
 from .version import app as version_app
 
 app = typer.Typer()
 
+app.add_typer(transforms_app, name="transforms")
 app.add_typer(version_app)
-app.add_typer(gun_app, name="gun")
 
-
-@app.callback()
-def callback():
-    """
-    Awesome Portal Gun
-    """
+if __name__ == "__main__":
+    app()
